36ee6a200a0d08c85b1c4c1c1c951027a7d975ce
Erick <Batista>
2021-01-30 20:58:27 -0300

    Up geral

---
+++ Projetos Erick/React/teste/src/components/form.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Formulario(){
+    return(
+        <>
+            <h1>Hello Word</h1>
+        </>
+    )
+}
---
+++ Projetos Erick/React/teste_real/src/components/Form/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Form(){
+    return(
+        <Form>
+            <input type="text" ></input>
+            <input type="text" ></input>
+            <input type="text" ></input>
+            <input type="text" ></input>
+        </Form>
+    )
+}
